0f3f399bf7b687b5f58f8a50741903615547a8d8
hassan <mhassanofficial7@gmail.com>
2024-04-15 10:53:45 +0500

    working on backend

--- backend/controllers/courseController.js
+++ backend/controllers/courseController.js
@@ -3,12 +3,63 @@ const courseModel = require('../models/course.model');
 const CustomError = require('../utils/customError');
 
 const getAllCourses = promiseHandler(async (req, res, next) => {
+    
     const courses = await courseModel.find().select('-lectures');
     
     res.status(200).json({
         success: true,
         message: courses,
     })
+});
+
+const getCourseLectures = promiseHandler(async (req, res, next) => {
+    const course = await courseModel.findById(req.params.id);
+    if(!course)
+    {
+        return next(new CustomError('Course not found!', 404));
+    }
+
+    course.views += 1;
+    await course.save();
+
+    res.status(200).json({
+        success: true,
+        lectures: course.lectures,
+    })
+})
+
+const addLectureToCourse = promiseHandler(async (req, res, next) => {
+    
+    const course = await courseModel.findById(req.params.id);
+    if(!course)
+    {
+        return next(new CustomError('Course not found!', 404));
+    }
+
+    const {title, description} = req.body;
+    //const file = req.file;
+
+    if(!title || !description)
+    {
+        return next(new CustomError('Please add all field!', 401));
+    }
+
+    course.lectures.push({
+        title,
+        description,
+        video:{
+            public_id: 'temp',
+            url: 'temp',
+        }
+    });
+    
+    course.totalVideos = course.lectures.length;
+    await course.save();
+
+    res.status(200).json({
+        success: true,
+        message: "Lecture has been added",
+    });
 });
 
 const createCourse = promiseHandler(async (req, res, next) => {
@@ -37,4 +88,4 @@ const createCourse = promiseHandler(async (req, res, next) => {
     })
 });
 
-module.exports = {getAllCourses, createCourse};
+module.exports = {getAllCourses, createCourse, getCourseLectures, addLectureToCourse};
--- backend/routes/courseRoutes.js
+++ backend/routes/courseRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const { getAllCourses, createCourse } = require('../controllers/courseController');
+const { getAllCourses, createCourse, getCourseLectures, addLectureToCourse } = require('../controllers/courseController');
 const courseRouter = express.Router();
 
 courseRouter.route('/courses').get(getAllCourses);
 
 courseRouter.route('/createcourse').post(createCourse);
+
+courseRouter.route('/course/:id')
+                .get(getCourseLectures)
+                .post(addLectureToCourse);
 
 module.exports = courseRouter;
